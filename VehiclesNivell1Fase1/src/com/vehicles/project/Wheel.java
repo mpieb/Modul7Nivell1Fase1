e8af7b0a4a2446e9274e75be3d2829412b8521f6
Miquel <76565907+mpieb@users.noreply.github.com>
2021-07-22 18:02:11 +0200

    Add files via upload

---
+++ VehiclesNivell1Fase1/src/com/vehicles/project/Wheel.java
@@ -0,0 +1,19 @@
+package com.vehicles.project;
+
+public class Wheel {
+	private String brand;
+	private double diameter;
+
+	public Wheel(String brand, double diameter) {
+		this.brand = brand;
+		this.diameter = diameter;
+	}
+
+	@Override
+	public String toString() {
+		return "Wheel [brand=" + brand + ", diameter=" + diameter + "]";
+	}
+
+}
+
+   
